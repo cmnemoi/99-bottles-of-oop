e9daf6bed28af7da8b6cf1e465b6beec77e7a283
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2026-01-11 09:55:32 +0100

    solution after 12 minutes

--- bottles.py
+++ bottles.py
@@ -6,31 +6,32 @@ class Bottles:
         return "\n".join([self.verse(n) for n in range(a, b - 1, -1)])
 
     def verse(self, n: int) -> str:
-        if n == 2:
-            return (
-                "2 bottles of beer on the wall, "
-                "2 bottles of beer.\n"
-                "Take one down and pass it around, "
-                "1 bottle of beer on the wall.\n"
-            )
-        if n == 1:
-            return (
-                "1 bottle of beer on the wall, "
-                "1 bottle of beer.\n"
-                "Take it down and pass it around, "
-                "no more bottles of beer on the wall.\n"
-            )
-        if n == 0:
-            return (
-                "No more bottles of beer on the wall, "
-                "no more bottles of beer.\n"
-                "Go to the store and buy some more, "
-                "99 bottles of beer on the wall.\n"
-            )
-        else:
-            return (
-                f"{n} bottles of beer on the wall, "
-                f"{n} bottles of beer.\n"
-                "Take one down and pass it around, "
-                f"{n - 1} bottles of beer on the wall.\n"
-            )
+        match n:
+            case 2:
+                return (
+                    "2 bottles of beer on the wall, "
+                    "2 bottles of beer.\n"
+                    "Take one down and pass it around, "
+                    "1 bottle of beer on the wall.\n"
+                )
+            case 1:
+                return (
+                    "1 bottle of beer on the wall, "
+                    "1 bottle of beer.\n"
+                    "Take it down and pass it around, "
+                    "no more bottles of beer on the wall.\n"
+                )
+            case 0:
+                return (
+                    "No more bottles of beer on the wall, "
+                    "no more bottles of beer.\n"
+                    "Go to the store and buy some more, "
+                    "99 bottles of beer on the wall.\n"
+                )
+            case _:
+                return (
+                    f"{n} bottles of beer on the wall, "
+                    f"{n} bottles of beer.\n"
+                    "Take one down and pass it around, "
+                    f"{n - 1} bottles of beer on the wall.\n"
+                )
--- bottles_test.py
+++ bottles_test.py
@@ -1,5 +1,3 @@
-import pytest
-
 from bottles import Bottles
 
 
