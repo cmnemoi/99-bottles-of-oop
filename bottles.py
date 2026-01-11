3d6112afb1c7bbb86a8f9723653010cda95728d1
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2026-01-11 09:53:10 +0100

    solution after 10minutes + epsilon

--- bottles.py
+++ bottles.py
@@ -6,13 +6,6 @@ class Bottles:
         return "\n".join([self.verse(n) for n in range(a, b - 1, -1)])
 
     def verse(self, n: int) -> str:
-        if n == 3:
-            return (
-                "3 bottles of beer on the wall, "
-                "3 bottles of beer.\n"
-                "Take one down and pass it around, "
-                "2 bottles of beer on the wall.\n"
-            )
         if n == 2:
             return (
                 "2 bottles of beer on the wall, "
