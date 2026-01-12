8b7080426cb1cbeeb93758b9c3e401c1cb75dc1b
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2026-01-12 23:28:15 +0100

    chapter 4.10

--- bottles.py
+++ bottles.py
@@ -6,36 +6,39 @@ class Bottles:
         return "\n".join([self.verse(n) for n in range(a, b - 1, -1)])
 
     def verse(self, n: int) -> str:
-        match n:
-            case 0:
-                return (
-                    "No more bottles of beer on the wall, "
-                    "no more bottles of beer.\n"
-                    "Go to the store and buy some more, "
-                    "99 bottles of beer on the wall.\n"
-                )
-            case _:
-                return (
-                    f"{n} {self._container(n)} of beer on the wall, "
-                    f"{n} {self._container(n)} of beer.\n"
-                    f"Take {self._pronoun(n)} down and pass it around, "
-                    f"{self._quantity(n - 1)} {self._container(n - 1)} of beer on the wall.\n"
-                )
+        return (
+            f"{self._quantity(n).capitalize()} {self._container(n)} of beer on the wall, "
+            f"{self._quantity(n)} {self._container(n)} of beer.\n"
+            f"{self._action(n)}, "
+            f"{self._quantity(self._successor(n))} {self._container(self._successor(n))} of beer on the wall.\n"
+        )
 
     def _container(self, n: int) -> str:
         if n == 1:
             return "bottle"
-        else:
-            return "bottles"
+
+        return "bottles"
 
     def _pronoun(self, n: int) -> str:
         if n == 1:
             return "it"
-        else:
-            return "one"
+
+        return "one"
 
     def _quantity(self, n: int) -> str:
         if n == 0:
             return "no more"
-        else:
-            return str(n)
+
+        return str(n)
+
+    def _action(self, n: int) -> str:
+        if n == 0:
+            return "Go to the store and buy some more"
+
+        return f"Take {self._pronoun(n)} down and pass it around"
+
+    def _successor(self, n: int) -> int:
+        if n == 0:
+            return 99
+
+        return n - 1
